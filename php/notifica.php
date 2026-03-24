44c4839d004360abfe09ada0f4a33c29e9645caa
ganttasticos3 <ganttasticos2@gmail.com>
2026-03-24 15:38:53 -0600

    Update notifica.php

--- php/notifica.php
+++ php/notifica.php
@@ -10,51 +10,49 @@ require_once __DIR__ . "/suscripcionElimina.php";
 use Minishlink\WebPush\WebPush;
 
 const AUTH = [
- "VAPID" => [
-  "subject" => "https://pentabyte02-github-io-1.onrender.com",
-  "publicKey" => "BGYcbx7KbK-vLvCGYTnl_kzmHZMMbs6wqf6HYF_Y4TEJyuQAu_27Yn2tBySI6xM8dlVWc4tXByZj7OGrcSTPZe0",
-  "privateKey" => "CtDPC1aZXh8Au8j2QVqvH8OqC6Aj5hSNfJCovfOP764"
- ]
+    "VAPID" => [
+        "subject" => "https://ganttasticos3-github-io.onrender.com",
+        "publicKey" => "BH3P1bFUPWjLhduRYdyax-ar70d_Rpn_plxlnKVwfOlGMX0apZlwMzZSyGGQkaGLfw7DFCGZH7akBfyYkMpw1t4",
+        "privateKey" => "eW9FSObHt2uBdBca9KSrSd_Rq3w89KLsntwaS3t4qCg"
+    ]
 ];
 
 $webPush = new WebPush(AUTH);
 
 $datos = json_decode(file_get_contents("php://input"), true);
-$texto = $datos["mensaje"] ?? "Mensaje vacío";
+$texto =  $datos["mensaje"] ?? "Mensaje vacío";
 
 $mensaje = json_encode([
     "title" => "Pentabyte 💬",
     "body" => $texto
 ]);
 
-// Envia el mensaje a todas las suscripciones.
-
 $bd = Bd::pdo();
 $stmt = $bd->query("SELECT * FROM SUSCRIPCION");
 $suscripciones =
- $stmt->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, Suscripcion::class);
+    $stmt->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, Suscripcion::class);
 
 foreach ($suscripciones as $suscripcion) {
- $webPush->queueNotification($suscripcion, $mensaje);
+    $webPush->queueNotification($suscripcion, $mensaje);
 }
 $reportes = $webPush->flush();
 
 // Genera el reporte de envio a cada suscripcion.
 $reporteDeEnvios = "";
 foreach ($reportes as $reporte) {
- $endpoint = $reporte->getRequest()->getUri();
- $htmlEndpoint = htmlentities($endpoint);
- if ($reporte->isSuccess()) {
-  // Reporte de éxito.
-  $reporteDeEnvios .= "<dt>$htmlEndpoint</dt><dd>Éxito</dd>";
- } else {
-  if ($reporte->isSubscriptionExpired()) {
-   suscripcionElimina($bd, $endpoint);
-  }
-  // Reporte de fallo.
-  $explicacion = htmlentities($reporte->getReason());
-  $reporteDeEnvios .= "<dt>$endpoint</dt><dd>Fallo: $explicacion</dd>";
- }
+    $endpoint = $reporte->getRequest()->getUri();
+    $htmlEndpoint = htmlentities($endpoint);
+    if ($reporte->isSuccess()) {
+        // Reporte de éxito.
+        $reporteDeEnvios .= "<dt>$htmlEndpoint</dt><dd>Éxito</dd>";
+    } else {
+        if ($reporte->isSubscriptionExpired()) {
+            suscripcionElimina($bd, $endpoint);
+        }
+        // Reporte de fallo.
+        $explicacion = htmlentities($reporte->getReason());
+        $reporteDeEnvios .= "<dt>$endpoint</dt><dd>Fallo: $explicacion</dd>";
+    }
 }
 
 devuelveJson(["reporte" => ["innerHTML" => $reporteDeEnvios]]);
