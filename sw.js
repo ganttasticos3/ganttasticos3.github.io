3a93b3b60d5ff705c1182bfc9c984a2452fba1f3
ganttasticos3 <ganttasticos2@gmail.com>
2026-03-25 00:15:22 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -60,8 +60,6 @@ const ARCHIVOS = [
   "img/maskable_icon_x512.png",
   "img/maskable_icon_x72.png",
   "img/maskable_icon_x96.png",
-  "img/screenshot_horizontal.png",
-  "img/screenshot_vertical.png",
   "img/Screenshot_horizontal_1.jpeg",
   "img/Screenshot_vertical_1.jpeg",
   "img/Screenshot_horizontal_2.jpeg",
